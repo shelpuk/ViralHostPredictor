8b9880b2329aa3c58019dfe5327d90e3af90246d
Sergii Shelpuk <tassadar.ha@gmail.com>
2020-03-25 18:22:11 +0200

    LightGBM initial commit

---
+++ reproduction/reproduction_lightgbm.py
@@ -0,0 +1,87 @@
+import lightgbm as lgb
+import numpy as np
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+import pandas as pd
+from scipy.stats import randint as sp_randint
+from scipy.stats import uniform as sp_uniform
+
+data = pd.read_csv('../BabayanEtAl_VirusData.csv', low_memory=False)
+
+experiment_data = data[data['Reservoir'].isin(['Artiodactyl',
+                                               'Carnivore',
+                                               'Fish',
+                                               'Galloanserae',
+                                               'Insect',
+                                               'Neoaves',
+                                               'Plant',
+                                               'Primate',
+                                               'Pterobat',
+                                               'Rodent',
+                                               'Vespbat'])]
+
+experiment_data = experiment_data.replace({'Reservoir': ['Artiodactyl', 'Carnivore', 'Fish', 'Galloanserae', 'Insect', 'Neoaves', 'Plant', 'Primate', 'Pterobat', 'Rodent', 'Vespbat']},
+                                          {'Reservoir': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]})
+
+# Verifying label replacement
+print(experiment_data.Reservoir.unique())
+print(experiment_data.shape)
+print(experiment_data[['Virus name', 'Reservoir']].head())
+
+train_set_frame = experiment_data.sample(frac=0.8, random_state=0)
+test_set_frame = experiment_data.drop(train_set_frame.index)
+
+train_labels = train_set_frame['Reservoir'].to_numpy()
+test_labels = test_set_frame['Reservoir'].to_numpy()
+
+train_set = train_set_frame.iloc[:, 6:].to_numpy()
+test_set = test_set_frame.iloc[:, 6:].to_numpy()
+
+train_data = lgb.Dataset(train_set, label=train_labels)
+test_data = lgb.Dataset(test_set, label=test_labels)
+
+# params = {"objective": "multiclass",
+#           "num_class": 11,
+#           "num_leaves": 60,
+#           "max_depth": -1,
+#           "learning_rate": 0.01,
+#           "bagging_fraction": 0.9,
+#           "feature_fraction": 0.9,
+#           "bagging_freq": 5,
+#           "bagging_seed": 2018,
+#           "verbosity": -1}
+#
+# num_round = 1000
+# bst = lgb.train(params, train_data, num_round, valid_sets=[test_data])
+#
+# predicted = bst.predict(test_set)
+#
+# print(np.sum(np.argmax(predicted, axis=1)==test_labels)/test_labels.shape)
+
+param_test = {'num_leaves': [8, 16, 24, 32, 40, 48, 56, 64],
+              'min_child_samples': [100, 200, 300, 400, 500],
+              'min_child_weight': [1e-5, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3, 1e4],
+              'subsample': [0.1, 0.15, 0.2, 0.25, 0.3],
+              'colsample_bytree': [0.2, 0.3, 0.4, 0.5, 0.6],
+              'reg_alpha': [0, 1e-1, 1, 2, 5, 7, 10, 50, 100],
+              'reg_lambda': [0, 1e-1, 1, 5, 10, 20, 50, 100],
+              'learning_rate': [0.0001, 0.001, 0.01, 0.1],
+              'bagging_fraction': [0.7, 0.8, 0.9, 0.95, 0.99],
+              'feature_fraction': [0.7, 0.8, 0.9, 0.95, 0.99],
+              'bagging_freq': [3, 4, 5, 6, 7]}
+
+fit_params = {'early_stopping_rounds': 30,
+              'eval_metric': 'multi_logloss',
+              'eval_set': [(test_set, test_labels)],
+              'verbose': 100}
+
+clf = lgb.LGBMClassifier(max_depth=-1, random_state=314, objective='multiclass', num_class=11, silent=True, metric='None', n_jobs=4, n_estimators=5000)
+gs = RandomizedSearchCV(estimator=clf, param_distributions=param_test,
+                        n_iter=500,
+                        scoring='roc_auc',
+                        cv=3,
+                        refit=True,
+                        random_state=314,
+                        verbose=True)
+
+gs.fit(train_set, train_labels, **fit_params)
+print('Best score reached: {} with params: {} '.format(gs.best_score_, gs.best_params_))
